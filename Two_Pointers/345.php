e824c738435c65d032c8077f588132a14c55324d
DerrickChen <derrickchen0915@gmail.com>
2024-07-23 10:16:19 -0400

    reorganize, 345 done

---
+++ Two_Pointers/345.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+    Given a string s, reverse only all the vowels in the string and return it.
+    The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+ */
+
+ class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function reverseVowels($s) {
+        $vowelsArray = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']; 
+        $i = 0;
+        $j = count(str_split($s)) - 1;
+
+        while($i < $j) {
+            if ( !in_array($s[$i], $vowelsArray) ) $i += 1;
+            else if ( !in_array($s[$j], $vowelsArray) ) $j -= 1;
+            else {
+                $temp = $s[$i];
+                $s[$i] = $s[$j];
+                $s[$j] = $temp;
+                $i += 1;
+                $j -= 1;
+            }
+        }  
+
+        return $s;
+    }
+}
